2ab5f57b34c23eee2556f3bf992ea50e2b1c2d4d
Nancy Lee <srlee713@gmail.com>
2019-09-02 17:54:09 -0600

    Add a test for displaying comments on article show page

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -4,15 +4,22 @@ describe "user sees one article" do
   describe "they link from the articles index" do
     it "displays info for one article" do
       article_1 = Article.create!(title: "Article 1", body: "Sample text")
+      comment_1 = article_1.comments.create(author_name: "Me", body: "Commenty comments")
+      comment_2 = article_1.comments.create(author_name: "You", body: "So much to say")
 
       visit articles_path
 
       click_link article_1.title
-
+    
       expect(current_path).to eq(article_path(article_1))
 
       expect(page).to have_content(article_1.title)
       expect(page).to have_content(article_1.body)
+      expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
+
     end
   end
 end

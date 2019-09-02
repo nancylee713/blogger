360a049c95aabc6bedf92025b7cfd1b3a38dabe7
Nancy Lee <srlee713@gmail.com>
2019-09-02 16:40:07 -0600

    Modify update action within articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,6 +31,7 @@ class ArticlesController < ApplicationController
 
   def update
     @article.update(article_params)
+    flash.notice = "Article #{@article.title} was updated."
     redirect_to article_path(@article)
   end
 

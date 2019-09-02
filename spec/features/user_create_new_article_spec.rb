ba4b28ad062f27724344db296ae46af33d6cc093
Nancy Lee <srlee713@gmail.com>
2019-09-02 14:40:21 -0600

    Add article create feature test

---
+++ spec/features/user_create_new_article_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "user create a new article" do
+  describe "they link from the articles index" do
+    describe "they fill in a title and body" do
+      it "creates a new article" do
+        visit articles_path
+        click_link "Create New Article"
+
+        expect(current_path).to eq(new_article_path)
+
+        fill_in "article[title]", with: "New title"
+        fill_in "article[body]", with: "New body"
+        click_on "Create Article"
+
+        expect(page).to have_content("New title")
+        expect(page).to have_content("New body")
+      end
+    end
+  end
+end

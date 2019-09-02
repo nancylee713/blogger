dda5a3de3dca97d22d13ae5723f89a926791ebcb
Nancy Lee <srlee713@gmail.com>
2019-09-02 12:31:59 -0600

    Add article migration

--- db/migrate/20190823183905_create_articles.rb
+++ db/migrate/20190823183905_create_articles.rb
@@ -3,6 +3,8 @@ class CreateArticles < ActiveRecord::Migration[5.2]
     create_table :articles do |t|
       t.string :title
       t.text :body
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_08_23_183905) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

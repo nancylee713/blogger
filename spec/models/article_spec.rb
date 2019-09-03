1e3e800425a87ace8308db656178a289298d11b0
Nancy Lee <srlee713@gmail.com>
2019-09-03 09:06:06 -0600

    Add a model test for tag_list instance method in article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -11,4 +11,14 @@ describe Article, type: :model do
     it {should have_many(:taggings)}
     it {should have_many(:tags).through(:taggings)}
   end
+
+  describe "instance methods" do
+    it "displays a list of tags" do
+      article = Article.create!(title: "Tall Tables", body: "They are tough for the short legged")
+      article.tags.create(name: "furniture")
+      article.tags.create(name: "opinions")
+
+      expect(article.tag_list).to eq("furniture, opinions")
+    end
+  end
 end

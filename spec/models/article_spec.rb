e151e3c2bf3d092cddfc67ff8a0689893e9a0b51
Nancy Lee <srlee713@gmail.com>
2019-09-04 12:55:43 -0600

    Add tag samples in article tag_list method test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -15,9 +15,8 @@ describe Article, type: :model do
   describe "instance methods" do
     it "displays a list of tags" do
       article = Article.create!(title: "Tall Tables", body: "They are tough for the short legged")
-      article.tags.create(name: "furniture")
-      article.tags.create(name: "opinions")
-
+      article.tags.create!(name: "furniture")
+      article.tags.create!(name: "opinions")
       expect(article.tag_list).to eq("furniture, opinions")
     end
   end

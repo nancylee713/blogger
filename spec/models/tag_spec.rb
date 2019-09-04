65c72a31a42b5b40ea2f77389669b1058332d67b
Nancy Lee <srlee713@gmail.com>
2019-09-04 12:56:17 -0600

    Add a model test for to_s method in tag model

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,4 +5,11 @@ describe Tag, type: :model do
     it {should have_many :taggings }
     it {should have_many(:articles).through(:taggings) }
   end
+
+  describe "instance methods" do
+    it "converts each tag to string" do
+      tag = Tag.create!(name: 'ruby, technology')
+      expect(tag.to_s).to eq('ruby, technology')
+    end
+  end
 end
